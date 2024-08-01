97849f74f94bbe8ff465c7b91641220fc6faff19
Ramazan <101656313+JED-Studio@users.noreply.github.com>
2024-08-01 06:40:06 +0300

    Добавлена функция для фильтрации и отображения вакансий по категориям
    - Реализована функция showCategory(category)
    - Функция очищает существующие вакансии в sectionJob
    - Осуществляется фильтрация массива items по указанной категории
    - Создаются и добавляются элементы вакансий для отфильтрованных элементов
    - Обеспечивается отображение только вакансий выбранной категории

--- js/text.js
+++ js/text.js
@@ -8,6 +8,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+      "li": [
+        ["Task 1a", "Task 1b", "Task 1c"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
+
     },
     {
       "id": 2,
@@ -17,6 +28,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+
+      "li": [
+        ["пашел ты"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
     },
     
     {
@@ -27,6 +49,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+
+      "li": [
+        ["пашел ты"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
     },
     
     {
@@ -37,6 +70,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+
+      "li": [
+        ["пашел ты"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
     },
     
     {
@@ -47,6 +91,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+
+      "li": [
+        ["пашел ты"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
     },
     
     {
@@ -57,6 +112,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+
+      "li": [
+        ["пашел ты"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
     },
     
     {
@@ -67,6 +133,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+
+      "li": [
+        ["пашел ты"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
     },
     {
       "id": 2,
@@ -76,6 +153,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+
+      "li": [
+        ["пашел ты"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
     },
     {
       "id": 2,
@@ -85,6 +173,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+
+      "li": [
+        ["пашел ты"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
     },
     {
       "id": 2,
@@ -94,6 +193,17 @@ document.addEventListener('DOMContentLoaded', function () {
       "imageUrl": "images/svg/icon 1.svg",
       "imageUrl1": "images/svg/icon 2.svg",
       "imageUrl2": "images/svg/icon 3.svg",
+      "h3": [
+        ["Task 1a",],
+        ["Responsibility 1a"],
+        ["Requirement 1a",]
+      ],
+
+      "li": [
+        ["пашел ты"],
+        ["Responsibility 1a", "Responsibility 1b", "Responsibility 1c"],
+        ["Requirement 1a", "Requirement 1b", "Requirement 1c"]
+      ]
     },
     
     
@@ -101,41 +211,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const sectionJob = document.getElementById('section_jobID');
 
-  
   function createJobPost(item) {
-    
     const jobPost = document.createElement('div');
     jobPost.className = 'section_job__post';
-    jobPost.style.cssText = 'padding: 30px 45px; display: flex; justify-content: space-between; align-items: center;'; 
+    jobPost.style.cssText = 'padding: 30px 45px; align-items: center;';
 
-    
+    const jobContainer = document.createElement('div');
+    jobContainer.className = 'section_job__container';
+    jobContainer.style.cssText = 'display: flex; align-items: center; justify-content: space-between; width: 100%; max-width: 745px;';
+
     const jobIcon = document.createElement('div');
     jobIcon.className = 'section_job__icon';
-    
 
-    
     const jobImg = document.createElement('div');
     jobImg.className = 'section_job__img';
     jobImg.style.cssText = 'display: flex; align-items: center;';
-
 
     const jobImg1 = document.createElement('div');
     jobImg1.className = 'section_job__img';
     jobImg1.style.cssText = 'display: flex; align-items: center; margin-top: 20px;';
 
-    
     const image = document.createElement('img');
     image.src = item.imageUrl;
     image.alt = 'Иконка';
     jobImg.appendChild(image);
 
-    
     const image1 = document.createElement('img');
     image1.src = item.imageUrl1;
     image1.alt = 'Иконка';
     jobImg1.appendChild(image1);
 
-    
     const title = document.createElement('p');
     title.style.cssText = 'margin-left: 20px; font-weight: 700; font-size: 28px; color: white;';
     title.textContent = item.title;
@@ -145,26 +250,46 @@ document.addEventListener('DOMContentLoaded', function () {
     title1.style.cssText = 'margin-left: 20px; font-weight: 700; font-size: 28px; color: #21A038;';
     title1.textContent = item.title1;
     jobImg1.appendChild(title1);
-    
+
     const jobImg2 = document.createElement('div');
     jobImg2.className = 'section_job__plus';
-    
+
     const image2 = document.createElement('img');
     image2.src = item.imageUrl2;
     image2.alt = 'Иконка';
     jobImg2.appendChild(image2);
-    
 
     jobIcon.appendChild(jobImg);
     jobIcon.appendChild(jobImg1);
-    jobPost.appendChild(jobIcon);
-    jobPost.appendChild(jobImg2);
-    
+
+    jobContainer.appendChild(jobIcon);
+    jobContainer.appendChild(jobImg2);
+
+    const textContainer = document.createElement('div');
+    textContainer.className = 'section_job__text-container';
+
+    item.h3.forEach((h3Array, index) => {
+      h3Array.forEach(h3Text => {
+          const h3 = document.createElement('h3');
+          h3.textContent = h3Text;
+          const ul = document.createElement('ul');
+          item.li[index].forEach(liText => {
+              const li = document.createElement('li');
+              li.textContent = liText;
+              ul.appendChild(li);
+          });
+          textContainer.appendChild(h3);
+          textContainer.appendChild(ul);
+      });
+  });
+  
+
+    jobPost.appendChild(jobContainer);
+    jobPost.appendChild(textContainer);
 
     return jobPost;
-  }
+}
 
- 
   function showCategory(category) {
     sectionJob.innerHTML = '';
     const filteredItems = items.filter(item => item.category === category);
@@ -183,5 +308,4 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   showCategory('construction');
-
 });
